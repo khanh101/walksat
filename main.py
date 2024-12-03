8f8e4985501578cd3e84a9efd0f9a11340e90699
khanh <khanh.nguyen.contact@gmail.com>
2024-12-04 04:08:38 +0800

    auto_push

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from walksat import c_walksat
 
-satisfiable, assignment = c_walksat(seed=12342, max_time_s=10, formula=[[1, -2], [2, 3]])
+satisfiable, assignment = c_walksat(seed=12342, max_time_s=10, formula=[[1, -2], [2, -1]])
 print(satisfiable, assignment)
